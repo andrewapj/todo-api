e8269612db96e11c66ae30c544be4db4024a3485
Andrew Johnson <andrew.apj@gmail.com>
2020-01-20 23:51:17 +0000

    - Added Javadoc

--- src/main/java/com/github/andrewapj/todoapi/api/model/EmptyResponse.java
+++ src/main/java/com/github/andrewapj/todoapi/api/model/EmptyResponse.java
@@ -2,6 +2,9 @@ package com.github.andrewapj.todoapi.api.model;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
+/**
+ * Empty JSON response.
+ */
 @JsonSerialize
 public class EmptyResponse {
 }
--- src/main/java/com/github/andrewapj/todoapi/service/query/impl/TodoListQueryServiceImpl.java
+++ src/main/java/com/github/andrewapj/todoapi/service/query/impl/TodoListQueryServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * Implementation of a {@link TodoListQueryService}.
+ */
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
